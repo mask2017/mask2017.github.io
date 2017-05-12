35a05011fd3066be1a0e8b8c691581fc3f284edf
mask2017 <krutu2017@mail.ru>
2017-05-13 02:46:13 +0700

    поправил отправку почты

---
+++ dist/config.php
@@ -0,0 +1,17 @@
+<?php
+
+// reCaptcha Setup
+// ===============
+
+// Mail Setup
+// ==========
+
+// Your Email Address where new emails will be sent to
+$to_email = 'kas.msk@gmail.com';
+
+// Email Subject
+$mail_subject = 'Заказ с сайта онлайнкасса';
+
+// Email content can be modified in the sendmail.php file.
+
+?>
---
+++ dist/sendmail.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'config.php';
+
+$json = array();
+$tel = isset( $_POST['tel'] ) ? $_POST['tel'] : '';
+$name = isset( $_POST['name'] ) ? $_POST['name'] : '';
+$message = isset( $_POST['message'] ) ? $_POST['message'] : '';
+
+if( !$name ) {
+    $json['error']['name'] = 'Пожалуйста, введите имя.';
+}
+if( !$message ) {
+    $json['error']['message'] = 'Пожалуйста, введите сообщение.';
+}
+if( !$tel || !preg_match('#^[-+0-9()\s]+$#', $tel ) ) {
+    $json['error']['tel'] = 'Пожалуйста, введите телефон.';
+}
+
+
+// If no errors
+if( !isset( $json['error'] ) ) {
+    // Email text
+    $mail_message = $name . "\r\n\r\n";
+    $mail_message .= $message. "\r\n\r\n";;
+    $mail_message .= $tel;
+    // Email title
+    $mail_headers  = "Content-type: text/plain; charset=utf-8\r\n";
+    $mail_headers .= "From: {$tel}\r\n";
+    // Sending email
+    mail( $to_email, $mail_subject, $mail_message, $mail_headers );
+    $json['success'] = 'Ваше сообщение успешно отправлено!';
+}
+
+echo json_encode( $json );
+?>
