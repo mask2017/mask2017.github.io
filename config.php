d0078562a1a11a2fee490d7b94568c2ee6b74a54
mask2017 <krutu2017@mail.ru>
2017-05-15 20:14:36 +0700

    дообавил почту

--- config.php
+++ config.php
@@ -9,7 +9,7 @@
 // Your Email Address where new emails will be sent to
 $to_email = 'kas.msk@gmail.com';
 $to1_email = 'ak@kkm-trade.ru';
-
+$to2_email = 'am@kkm-trade.ru';
 // Email Subject
 $mail_subject = 'Заказ с сайта онлайнкасса';
 
